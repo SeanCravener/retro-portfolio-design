92e3dd9218befeb7577f3142e2b31f4bbedca31f
Sean Cravener <scravenerpat@gmail.com>
2023-02-15 15:13:35 -0500

    Set up code to test switching bg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [themeStyle, setThemeStyle] = useState("background-one");
+
+  return (
+    <div
+      className={themeStyle}
+      style={{ textAlign: "center", paddingTop: "5em" }}
+    >
+      <button
+        onClick={() =>
+          themeStyle === "background-one"
+            ? setThemeStyle("background-two")
+            : setThemeStyle("background-one")
+        }
+        style={{ backgroundColor: "blue" }}
+      >
+        Click me!
+      </button>
+    </div>
+  );
 }
 
 export default App;
