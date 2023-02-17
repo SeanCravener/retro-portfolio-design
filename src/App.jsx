ce1d5977075904553a0204a14f6e52772082d076
Sean Cravener <scravenerpat@gmail.com>
2023-02-17 17:45:49 -0500

    Edited for testing, change later

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import ContentBoxContainer from "./components/content-box/content-box-container";
+import MainContainer from "./components/background/background";
+import Frame from "./components/frame/frame";
 import ContentBox from "./components/content-box/content-box";
 
 function App() {
-  const stringForTesting =
-    "Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower. Productize. Optics accountable talk. Thought shower.";
-
+  
   return (
-    <div className="App">
-      <ContentBoxContainer>
-        <ContentBox>{stringForTesting}</ContentBox>
-      </ContentBoxContainer>
-    </div>
+    <MainContainer themeClassName={"vaporwave-two"}>
+      <Frame frameType={"frame-outer"}>
+        <Frame frameType={"frame-inner"}>
+          <ContentBox>
+            <h1>Content Box</h1>
+          </ContentBox>
+          <ContentBox>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum ex est provident porro eius dolor repudiandae dolores quo laboriosam magnam cum maxime sit quibusdam voluptate laudantium voluptatibus totam, quas explicabo!</p>
+          </ContentBox>
+        </Frame>
+      </Frame>
+    </MainContainer>
   );
 }
 
